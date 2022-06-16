071bdf01be8cbd6969fdc485cd6d5660d3469d74
Marco Antonio Sydney Horta <marco.sh@outlook.com>
2022-06-16 11:37:50 -0300

    Add project files.

---
+++ Exception/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Type a number, type enter, and then another and type enter (try a letter or second number equal zero): ");
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division by zero is not allowed");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error! " + e.Message);
+            }
+        }
+    }
+}
